b555166659cd017e8268d8b9823a340f7e9450bd
davinci8gilbert <davinci8gilbert@yahoo.com>
2023-04-08 11:37:13 -0700

    additionaleditissue

--- src/components/IssueEdit.vue
+++ src/components/IssueEdit.vue
@@ -32,14 +32,36 @@
                 <br>        
                 <label class="label" for="personresponsible">Person Responsible: </label>
                 <br>
-                <input class="inputtext" type="inputtext" name="personresponsibletext" id="personresponsibleid" placeholder="" v-model="personresponsible">       
+                <input class="inputtext" type="inputtext" name="personresponsibletext" id="personresponsibleid" placeholder="" v-model="personresponsible" readonly>       
                 <br>        
                 <label class="label" for="emailresponsible">E-mail Address: </label>
                 <br>
                 <input class="inputtext" type="inputtext" name="emailtext" id="emailid" placeholder="" v-model="email">
                 <br>
             </div>
+            <br>
+            <br>
+            
     </div>
+            <div class="issueupdate">
+                <h3>Updates Of Issue</h3>
+                <label class="label" for="actionsTaken">New Action Taken: </label>
+                <br>
+                <textarea class="textarea"  type="inputtext" name="deptresponsibletext" id="deptresponsibleid" placeholder="" rows="5" cols="50" v-model="newAction"></textarea>
+                <br> 
+                <br>       
+                <label class="label" for="personresponsible">New Timeline: </label>
+                <br>
+                <input class="inputtext" type="date" name="newtimeline" id="newtimelineid" placeholder="" v-model="newTimeline">       
+                <br>   
+                <br>
+                <label class="label" for="personresponsible">Status of Issue: </label>
+                <br>
+                <select class="issuestatus" name="issuestatus" v-model="issueStatus" @change="handlerStatus">
+                    <option value="outstanding">Outstanding</option>
+                    <option value="closed">Closed</option>
+                </select>
+            </div>
     </div>
 
 </template>
@@ -55,7 +77,12 @@
             issueDateNC:"",
             approvedDateNC:"",
             personresponsible:"",
-            email:""
+            email:"",
+            newAction:"",
+            newTimeline:"",
+            issueStatus:"",
+            issueStatusSelection:"",
+            dateOfEdit:""
           }
             
         },
@@ -83,7 +110,13 @@
             },
 
             getEmail(){
-                this.email = this.issueDetail.auditee.employee.email
+                this.email = this.issueDetail.auditee.employee.email;
+                this.personresponsible=this.issueDetail.auditee.employee.firstName+" "+this.issueDetail.auditee.employee.lastName;
+            },
+            handlerStatus(event){
+                event.preventDefault();
+                this.issueStatusSelection = this.issueStatus;
+                console.log(this.issueStatusSelection);
             }
         },
         
