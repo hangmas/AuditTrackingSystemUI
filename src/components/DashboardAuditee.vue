41305b813aeb916b7b0b90769f0a71e29f4b641d
arifinw <arifinw@gmail.com>
2023-04-11 17:15:35 -0700

    commit #12

--- src/components/DashboardAuditee.vue
+++ src/components/DashboardAuditee.vue
@@ -1,77 +1,62 @@
 <template>
-    <div>
-        <h1>Auditee Dashboard</h1>
-        <h2>Outstanding Issues</h2>
-        <table class="issuelisttable">
-        <tr>
-          <th>Report Title</th>
-          <th>Issue Title</th>
-          <th>Issue Date</th>
-          <th>Risk Rating</th>
-          <th>Departement Responsible</th>
-          <th>Approved Deadline</th>
-        </tr>
-        <tr v-for="(item, index) in filteredIssues" :key="index">
-              <td>{{ item.reportTitle }}</td>
-              <td>{{ item.issueTitle }}</td>
-              <td>{{ item.issueDate }}</td>
-              <td>{{ item.riskRating }}</td>
-              <td>{{ item.departmentResponsible }}</td>
-              <td>{{ item.approvedDeadline }}</td>
-            </tr>
-    </table>
-    </div>
-    <br><br>
-    <div>
-        <h2>Closed Issues</h2>
-        <table class="issuelisttable">
-        <tr>
-          <th>Report Title</th>
-          <th>Issue Title</th>
-          <th>Issue Date</th>
-          <th>Risk Rating</th>
-          <th>Departement Responsible</th>
-          <th>Approved Deadline</th>
-        </tr>
-        <tr v-for="(item, index) in filteredIssues2" :key="index">
-              <td>{{ item.reportTitle }}{{ item.auditee.employee.department }}</td>
-              <td>{{ item.issueTitle }}</td>
-              <td>{{ item.issueDate }}</td>
-              <td>{{ item.riskRating }}</td>
-              <td>{{ item.departmentResponsible }}</td>
-              <td>{{ item.approvedDeadline }}</td>
-            </tr>
-    </table>
-    </div>
-
-    
-    <!--
-    <div>
-      <p v-for="(item, index) in filteredEmp" :key="index" :value="firstName">{{ item.firstName }}</p>
-    </div>-->
-    <!--
-    <div>
-      <ul>
-        <li v-for="(employee, index) in employee" :key="index">{{ employee.firstName }}</li>
-        </ul>
-      </div>-->
-    </template>
-    <script>
-    import EmployeeDataService from '../services/EmployeeDataService'
-    import IssueService from '../services/IssueService'
-    
-    export default{
-    
-    name: 'dashboardAuditee',
-    data()
-    {
-        return{
-          
-          eDepartment :"",
-          selectedDepartment: "",
-          selectedDepartment2: "",
-          employee : [],
-          issuesList:[],
+  <div>
+    <header>
+      <h1>AUDIT ISSUES MONITORING SYSTEM</h1>
+    </header>
+    <nav>
+      <ul class="nav-links">
+    <li><Router-link to="" @click="toDashboard">Dashboard</Router-link></li>
+    <li><Router-link to="" @click="toIssue">Issues</Router-link></li>
+    <li><Router-link to="" @click="toReport">Report</Router-link></li>
+    <li><Router-link to="" @click="toSettings">Settings</Router-link></li>
+    <li><Router-link to="" @click="toLogout">Logout</Router-link></li>
+  </ul>
+  </nav>
+  </div>
+  <div>
+      <h1>Auditee Dashboard</h1>
+      <h2>Outstanding Issues</h2>
+      <table class="issuelisttable">
+      <tr>
+        <th>Report Title</th>
+        <th>Issue Title</th>
+        <th>Issue Date</th>
+        <th>Risk Rating</th>
+        <th>Departement Responsible</th>
+        <th>Approved Deadline</th>
+      </tr>
+      <tr v-for="(item, index) in filteredIssues" :key="index">
+            <td>{{ item.reportTitle }}</td>
+            <td>{{ item.issueTitle }}</td>
+            <td>{{ item.issueDate }}</td>
+            <td>{{ item.riskRating }}</td>
+            <td>{{ item.departmentResponsible }}</td>
+            <td>{{ item.approvedDeadline }}</td>
+          </tr>
+  </table>
+  </div>
+  <br><br>
+  <div>
+      <h2>Closed Issues</h2>
+      <table class="issuelisttable">
+      <tr>
+        <th>Report Title</th>
+        <th>Issue Title</th>
+        <th>Issue Date</th>
+        <th>Risk Rating</th>
+        <th>Departement Responsible</th>
+        <th>Approved Deadline</th>
+      </tr>
+      <tr v-for="(item, index) in filteredIssues2" :key="index">
+            <td>{{ item.reportTitle }}{{ item.auditee.employee.department }}</td>
+            <td>{{ item.issueTitle }}</td>
+            <td>{{ item.issueDate }}</td>
+            <td>{{ item.riskRating }}</td>
+            <td>{{ item.departmentResponsible }}</td>
+            <td>{{ item.approvedDeadline }}</td>
+          </tr>
+  </table>
+  </div>
 
   </template>
   <script>
@@ -107,6 +92,7 @@ import EmployeeDataService from '../services/EmployeeDataService'
               .then(response=> {
                 this.employee = response.data; //employee will save all the data 
                   console.log(this.employee);
+                  console.log(this.eid);
                   this.employee.forEach(item => {     
                     if(item.employee.id === parseInt(this.eid))
                     {          
@@ -128,7 +114,7 @@ import EmployeeDataService from '../services/EmployeeDataService'
           toIssue(event)
           {
             event.preventDefault();
-            this.$router.push({name:"IssuePageAuditee"});
+            this.$router.push({name:"IssuesPage"});
 
           },
 
@@ -170,8 +156,7 @@ import EmployeeDataService from '../services/EmployeeDataService'
           retrieveIssue(){
           IssueService.getIssues()
               .then(response =>{
-                  this.issuesList = response.data;
-                  console.log(this.issuesList);                   
+                  this.issuesList = response.data;                  
                   this.issuesList.forEach(issue => {
                   const date = new Date(issue.issueDate * 1000);
                   issue.issueDate = date.toLocaleDateString(); // update issueDate to the formatted date string
