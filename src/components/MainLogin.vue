b9792b700b21d05cc13b70b3e39ab0dd4817028a
hangmas <rais19@student.douglascollege.ca>
2023-04-13 14:28:22 -0700

    made changes in UI

--- src/components/MainLogin.vue
+++ src/components/MainLogin.vue
@@ -5,78 +5,79 @@
 
             <div>
                 <label for=" email">Email:</label>
-                <input type="text" id="email" v-model="employeeLoginRequest.email"/>
+                <input type="text" id="email" v-model="employeeLoginRequest.email" />
 
             </div>
             <div>
                 <label for=" password">Password:</label>
-                <input type="password" id="password" v-model="employeeLoginRequest.password"/>
+                <input type="password" id="password" v-model="employeeLoginRequest.password" />
             </div>
             <div>
                 <button type="submit" @click="login">Login</button>
             </div>
         </form>
         <p>
-                    <router-link to="/signup">Sign Up</router-link>
-                </p>
+            <router-link to="/setPassword">Set Password</router-link>
+        </p>
 
         <p>{{ message }}</p>
     </div>
-
-
-
 </template>
 
 <script>
 import LoginService from "../services/LoginService";
 import '../App.css'
 
-export default{
-    name : "mainLogin",
-    data(){
-        return{
-            employeeLoginRequest: {email:"", password:""},
+export default {
+    name: "mainLogin",
+    data() {
+        return {
+            employeeLoginRequest: { email: "", password: "" },
             message: ""
         }
 
     },
-    methods:{
-        login(event){
+    methods: {
+        login(event) {
             event.preventDefault();
-            LoginService.employeeLogin(this.employeeLoginRequest)
-            .then(response => {
-                let item = response.data;
-                localStorage.setItem('eid',item.employee.id);
-                localStorage.setItem('role',item.role);
-                if (item.role === 1 || item.role ===2){
-                    this.$router.push({ name: "auditorNavigation"});
-                }
-                else {
-                    this.$router.push({ name: "auditeeNavigation"});
+            if (this.employeeLoginRequest.email === "" || this.employeeLoginRequest.password === "") {
+                window.alert("Please enter your credentials");
+            } else {
+                LoginService.employeeLogin(this.employeeLoginRequest)
+                    .then(response => {
+                        let item = response.data;
+                        localStorage.setItem('eid', item.employee.id);
+                        localStorage.setItem('role', item.role);
+                        if (item.role === 1 || item.role === 2) {
+                            this.$router.push({ name: "auditorNavigation" });
+                        }
+                        else {
+                            this.$router.push({ name: "auditeeNavigation" });
 
-                }
-                
-            })
-            .catch(error => {
-                this.employeeLoginRequest.email = "";
-                this.employeeLoginRequest.password = "";
-                this.message = error.response.data.message;
-                console.log(error.response.data);
-            })
+                        }
+
+                    })
+                    .catch(error => {
+                        this.employeeLoginRequest.email = "";
+                        this.employeeLoginRequest.password = "";
+                        this.message = error.response.data.message;
+                        window.alert("Some of the credentials are wrong. Try again please");
+                        console.log(error.response.data);
+                    })
+            }
+
 
 
         }
 
     },
 
-mounted(){
+    mounted() {
 
-    this.message = "";
+        this.message = "";
 
-}
+    }
 }
 
 </script>
-<style>
-
-</style>
+<style></style>
